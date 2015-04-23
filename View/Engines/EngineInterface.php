2b4f2f6f1db10f133d990ddeccd70165f11a426e
chuan-hao, you <howtomakeaturn@hotmail.com>
2015-04-23 22:16:18 +0800

    php engine.

--- View/Engines/EngineInterface.php
+++ View/Engines/EngineInterface.php
@@ -9,6 +9,9 @@ interface EngineInterface {
 	 * @param  array   $data
 	 * @return string
 	 */
+     // 喔喔，「引擎介面」就是模板引擎介面喔。
+     
+     // 快講你的檔案路徑跟要塞的資料啦！
 	public function get($path, array $data = array());
 
 }
--- View/Engines/PhpEngine.php
+++ View/Engines/PhpEngine.php
@@ -1,5 +1,7 @@
 <?php namespace Illuminate\View\Engines;
+// 大膽預測：既然這個php engine是用來處理.php檔的，它八成沒做多少狗屁事
 
+// 它八成大方回傳內容而已！因為PHP本身就是你他媽的PHP引擎阿
 class PhpEngine implements EngineInterface {
 
 	/**
@@ -14,6 +16,9 @@ class PhpEngine implements EngineInterface {
 		return $this->evaluatePath($path, $data);
 	}
 
+    // 這個雞巴函式跟get根本一模一樣。註解還可以不一樣，靠！
+    
+    // 就承認get是為了對使用者友善而存在就好了嘛！
 	/**
 	 * Get the evaluated contents of the view at the given path.
 	 *
@@ -23,6 +28,9 @@ class PhpEngine implements EngineInterface {
 	 */
 	protected function evaluatePath($__path, $__data)
 	{
+        // 果然沒做多少雞巴事
+        
+        // 只是先開再關php的output buffering而已
 		$obLevel = ob_get_level();
 
 		ob_start();
@@ -55,6 +63,9 @@ class PhpEngine implements EngineInterface {
 	 */
 	protected function handleViewException($e, $obLevel)
 	{
+        // 隨便啦，反正這個函式就是把遇到的例外...用力搞定啦
+        
+        // 哈哈
 		while (ob_get_level() > $obLevel)
 		{
 			ob_end_clean();
