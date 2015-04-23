ab89d60f236dedfc38abd4089c1dfecf0e66a464
chuan-hao, you <howtomakeaturn@hotmail.com>
2015-04-23 22:07:33 +0800

    mm

--- View/Engines/EngineResolver.php
+++ View/Engines/EngineResolver.php
@@ -29,6 +29,9 @@ class EngineResolver {
 	 */
 	public function register($engine, Closure $resolver)
 	{
+        // 喔，誤會一場，EngineResolver根本不是什麼IoC container
+        
+        // 只是把東西放進一個陣列。算是他媽的簡易版的IoC container
 		$this->resolvers[$engine] = $resolver;
 	}
 
@@ -41,6 +44,7 @@ class EngineResolver {
 	 */
 	public function resolve($engine)
 	{
+        // resolve...解析什麼鬼啊。只是把東西從陣列拿出來，幹
 		if (isset($this->resolved[$engine]))
 		{
 			return $this->resolved[$engine];
@@ -48,10 +52,12 @@ class EngineResolver {
 
 		if (isset($this->resolvers[$engine]))
 		{
+            // call_user_func可以這樣用喔！可以用在Closure身上，執行一段動作ㄝ。真他媽的帥氣
 			return $this->resolved[$engine] = call_user_func($this->resolvers[$engine]);
 		}
-
+        // 好啦反正resolve就是希望每個resolver只會執行一次。之後直接從memory裡面叫出來。
 		throw new \InvalidArgumentException("Engine $engine not found.");
 	}
 
 }
+// 我知道這個Resolver只有存放PhpEngine跟CompilerEngine啦，接下來就看它們
